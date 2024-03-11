e6256a65bc9cb2cd61ef7c3fe418dbbea673d439
rahulprasad01 <rahulprasad2001@gmail.com>
2024-03-11 12:14:11 +0800

    Update product

--- client/src/App.js
+++ client/src/App.js
@@ -17,31 +17,33 @@ import CreateProduct from "./pages/admin/CreateProduct";
 import Users from "./pages/admin/Users";
 import Orders from "./pages/user/Orders";
 import Profile from "./pages/user/Profile";
-
+import Products from "./pages/admin/Products";
+import UpdateProduct from "./pages/admin/UpdateProduct";
 function App() {
   return (
     <>
-    <Routes>
-     <Route path="/" element={<HomePage />} />
-     <Route path="/dashboard" element={<PrivateRoute />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
           <Route path="user/orders" element={<Orders />} />
           <Route path="user/profile" element={<Profile />} />
-     </Route>
-     <Route path="/dashboard" element={<AdminRoute />}>
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/product/:slug" element={<UpdateProduct />} />
+          <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users />} />
-     </Route>
-     <Route path="/register" element={<Register />} />
-     <Route path="/login" element={<Login />} />
-     <Route path="/about" element={<About />} />
-     <Route path="/contact" element={<Contact />} />
-     <Route path="/policy" element={<Policy />} />
-     <Route path="/*" element={<Pagenotfound />} />
-    </Routes>
-    
+        </Route>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
